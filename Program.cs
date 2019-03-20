9a49f809426968591e9838cef30231314dbb6049
Edgar Ochoa <eochoa3@ucol.mx>
2019-03-20 00:32:10 -0600

    02. Leer casos de prueba

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 
 namespace automatizar_pruebas_unitarias_2019_edgar8acas
 {
@@ -6,7 +8,23 @@ namespace automatizar_pruebas_unitarias_2019_edgar8acas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList testCases = new ArrayList();
+            try
+            {
+                using(StreamReader sr = new StreamReader("CasosPrueba.txt")) 
+                {
+                    string line;
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        testCases.Add(line);
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Error al leer el archivo: " + e.Message);
+            }
         }
     }
 }

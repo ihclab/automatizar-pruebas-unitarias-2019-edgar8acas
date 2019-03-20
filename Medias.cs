19cf6c753cd439b5f5fdd73f8d80e4e6047f57f7
Edgar Ochoa <eochoa3@ucol.mx>
2019-03-20 00:18:15 -0600

    solve return functions problem

--- Medias.cs
+++ Medias.cs
@@ -11,22 +11,29 @@ namespace AutomatizarPruebasUnitarias {
         /**
          * Calcula y regresa la media artimética
          */
-         // Silly comment
-        public static double mediaAritmetica(params int[] vals) { }
+        public static double mediaAritmetica(params int[] vals) { 
+            return 0;
+        }
 
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
-        private static double raizEnesima(double x, int n) { }
+        private static double raizEnesima(double x, int n) { 
+            return 0;
+        }
 
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
          */
-        public double mediaGeometrica(params int[] vals) { }
+        public double mediaGeometrica(params int[] vals) { 
+            return 0;
+        }
 
         /**
          * Este método no está implementado.
          */
-        public static double mediaArmonica(params int[] vals) { }
+        public static double mediaArmonica(params int[] vals) { 
+            return 0;
+        }
     }
 }

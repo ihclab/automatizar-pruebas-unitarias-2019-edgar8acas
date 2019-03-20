4c37ca18c6e355a358daf781993f2fc0a75a1b06
Edgar Ochoa <eochoa3@ucol.mx>
2019-03-20 00:11:54 -0600

    01.1 Added dotnet console project and gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace automatizar_pruebas_unitarias_2019_edgar8acas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
